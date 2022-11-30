4f762ef84ab4456b31e212350b26effc33e79f86
Islam Ibrahim <islam-ibrahim-ii@outlook.com>
2022-11-30 10:06:55 +0200

    Reverse engineering for test database

--- ConsolidationTool.Api/Program.cs
+++ ConsolidationTool.Api/Program.cs
@@ -1,13 +1,28 @@
+using ConsolidationTool.Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TestDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TestDBContext>();
+
+    context.Database.Migrate();
+};
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
