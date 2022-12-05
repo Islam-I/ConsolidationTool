f67a7bd5e328fd05943548c3a5bd4df3e4263800
Islam Ibrahim <islam-ibrahim-ii@outlook.com>
2022-12-05 13:49:33 +0200

    Add TestDev models and migration

---
+++ ConsolidationTool.Data/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolidationTool.Data.Models
+{
+    public class Category : CommonProperties
+    {
+        public Category()
+        {
+            SubCategory = new HashSet<SubCategory>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public virtual ICollection<SubCategory> SubCategory { get; set; }
+    }
+}
---
+++ ConsolidationTool.Data/Models/SubCategory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolidationTool.Data.Models
+{
+    public class SubCategory : CommonProperties
+    {
+        public SubCategory()
+        {
+            Property = new HashSet<Property>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Property> Property { get; set; }
+    }
+}
