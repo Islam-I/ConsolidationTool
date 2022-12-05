057a88eb0db987540f496a065f8d86c2da015137
Islam Ibrahim <islam-ibrahim-ii@outlook.com>
2022-12-05 16:13:06 +0200

    Update Program.cs

--- ConsolidationTool.Api/Program.cs
+++ ConsolidationTool.Api/Program.cs
@@ -62,6 +62,7 @@ builder.Services.AddAuthentication(options =>
 
 
 
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
