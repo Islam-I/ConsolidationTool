05ea16e377dd574ee811a4a1d5c313cfb91903f9
Islam Ibrahim <islam-ibrahim-ii@outlook.com>
2022-12-05 15:43:38 +0200

    Revert "Conflict"
    This reverts commit fd70bf8bb292690011f4c897d14f348ad596ad75, reversing
    changes made to 755d80a1969776512b29658a906940167e16b03b.

--- ConsolidationTool.Api/Program.cs
+++ ConsolidationTool.Api/Program.cs
@@ -1,27 +1,8 @@
-using ConsolidationTool.Data.DatabaseContext;
-using ConsolidationTool.Data.Models;
-using ConsolidationTool.Repository.UnitOfWork;
-using ConsolidationTool.Service.Helpers;
-using ConsolidationTool.Service.Interfaces;
-using ConsolidationTool.Service.Interfaces.UserManagement;
-using ConsolidationTool.Service.Services;
-using ConsolidationTool.Service.Services.UserManagement;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<TestDBContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -35,17 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors(x =>
-{
-    x.WithOrigins("http://127.0.0.1:4200");
-    x.AllowAnyHeader();
-    x.AllowAnyMethod();
-    x.AllowCredentials();
-});
-
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
