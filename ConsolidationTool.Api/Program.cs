f802d4006f621120c8bc50d03e74444a459760e6
Islam Ibrahim <islam-ibrahim-ii@outlook.com>
2022-12-05 15:58:30 +0200

    Revert "Merge branch 'main' into TestDev"
    This reverts commit 72a9c39e19bca4d9b4303f060221926f9adf4364, reversing
    changes made to 5aeaae5beacf8a7c12b3cf08825c84c4138a411c.

--- ConsolidationTool.Api/Program.cs
+++ ConsolidationTool.Api/Program.cs
@@ -22,11 +22,57 @@ builder.Services.AddDbContext<TestDBContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+
+
+builder.Services.AddScoped<IAccountServices, AccountSerivces>();
+builder.Services.AddScoped<IAuthenticationServices, AuthenticationServices>();
+builder.Services.Configure<JwtModel>(builder.Configuration.GetSection("Jwt"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = false;
+})
+    .AddEntityFrameworkStores<TestDBContext>()
+    .AddDefaultTokenProviders();
+// Adding Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+// Adding Jwt Bearer
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<TestDBContext>();
+
+    context.Database.Migrate();
+};
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
