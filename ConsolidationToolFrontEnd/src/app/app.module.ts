f3c126701a9d6b5f9d078687dfddf8c4011540c5
YehiaAli <YehiaAli@YEHIA-ALI>
2022-12-12 02:02:55 +0200

    Front End

--- ConsolidationToolFrontEnd/src/app/app.module.ts
+++ ConsolidationToolFrontEnd/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { MaterialModule } from './modules/material/material.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
+    // declarations Use When use Component and Directive , Pipe
     AppComponent
   ],
   imports: [
+    // import All Module
     MaterialModule,
     BrowserModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    // providers Use When auth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
